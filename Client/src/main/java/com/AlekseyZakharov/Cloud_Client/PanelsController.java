f4efe3daca90093d0775cdeaa7d6d9949e5eb95b
Aleksey_Zakharov <a.zakharov@yandex.ru>
2020-09-08 10:45:18 +0300

    Initial commit

---
+++ Client/src/main/java/com/AlekseyZakharov/Cloud_Client/PanelsController.java
@@ -0,0 +1,86 @@
+package com.AlekseyZakharov.Cloud_Client;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.*;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+public class PanelsController implements Initializable {
+
+    public TableView<FileInfo> filesTable;
+    public ComboBox<Path> disksBox;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        TableColumn<FileInfo, String> fileTypeColumn = new TableColumn<>();
+        fileTypeColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getType().getName()));
+        fileTypeColumn.setPrefWidth(24);
+
+        TableColumn<FileInfo, String> fileNameColumn = new TableColumn<>("Имя");
+        fileNameColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getFileName()));
+        fileNameColumn.setPrefWidth(240);
+
+        TableColumn<FileInfo, Long> fileSizeColumn = new TableColumn<>("Размер");
+        fileSizeColumn.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue().getSize()));
+        fileSizeColumn.setPrefWidth(120);
+        fileSizeColumn.setCellFactory(column -> {
+            return new TableCell<FileInfo, Long>() {
+                @Override
+                protected void updateItem(Long item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || empty) {
+                        setText(null);
+                        setStyle("");
+                    } else {
+                        String text = String.format("%, d bytes", item);
+                        if (item == -1L) {
+                            text = "[DIR]";
+                        }
+                        setText(text);
+                    }
+                }
+            };
+        });
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd  hh:mm:ss");
+        TableColumn<FileInfo, String> fileLastModifiedColumn = new TableColumn<>("Время изменения");
+        fileLastModifiedColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getLastModified().format(dtf)));
+        fileLastModifiedColumn.setPrefWidth(120);
+
+        filesTable.getColumns().addAll(fileTypeColumn, fileNameColumn, fileSizeColumn, fileLastModifiedColumn);
+
+        updateFilesTable(Paths.get("C:"));
+        filesTable.getSortOrder().add(fileTypeColumn);
+
+        disksBox.getItems().clear();
+        for (Path p : FileSystems.getDefault().getRootDirectories()) {
+            disksBox.getItems().add(p);
+        }
+        disksBox.getSelectionModel().selectFirst();
+    }
+
+    public void updateFilesTable(Path path) {
+        filesTable.getItems().clear();
+        try {
+            filesTable.getItems().addAll(Files.list(path).map(FileInfo::new).collect(Collectors.toList()));
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Не удалось обновить список файлов", ButtonType.APPLY);
+            alert.showAndWait();
+        }
+        filesTable.sort();
+    }
+
+    public void selectDiskAction(ActionEvent actionEvent) {
+        ComboBox<String> element = (ComboBox<String>) actionEvent.getSource();
+        updateFilesTable(Paths.get(element.getValue()));
+    }
+}
