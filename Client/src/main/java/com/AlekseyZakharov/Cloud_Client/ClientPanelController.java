9e4366d905507e6bc7924c783b37b99335515125
Aleksey_Zakharov <a.zakharov@yandex.ru>
2020-09-15 13:53:12 +0300

    Revert "Почти сформированный GUI"
    This reverts commit 962e49bd

--- Client/src/main/java/com/AlekseyZakharov/Cloud_Client/ClientPanelController.java
+++ Client/src/main/java/com/AlekseyZakharov/Cloud_Client/ClientPanelController.java
@@ -5,7 +5,7 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
+
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.*;
@@ -17,7 +17,6 @@ public class ClientPanelController implements Initializable {
 
     public TableView<FileInfo> filesTable;
     public ComboBox<Path> disksBox;
-    public TextField pathField;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -59,17 +58,17 @@ public class ClientPanelController implements Initializable {
 
         filesTable.getColumns().addAll(fileTypeColumn, fileNameColumn, fileSizeColumn, fileLastModifiedColumn);
 
-        updateFilesTable(Paths.get("."));
+        updateFilesTable(Paths.get("C:"));
         filesTable.getSortOrder().add(fileTypeColumn);
 
         disksBox.getItems().clear();
         for (Path p : FileSystems.getDefault().getRootDirectories()) {
             disksBox.getItems().add(p);
         }
+        disksBox.getSelectionModel().selectFirst();
     }
 
     public void updateFilesTable(Path path) {
-        pathField.setText(path.normalize().toAbsolutePath().toString());
         filesTable.getItems().clear();
         try {
             filesTable.getItems().addAll(Files.list(path).map(FileInfo::new).collect(Collectors.toList()));
@@ -81,35 +80,7 @@ public class ClientPanelController implements Initializable {
     }
 
     public void selectDiskAction(ActionEvent actionEvent) {
-        ComboBox<Path> element = (ComboBox<Path>) actionEvent.getSource();
-        updateFilesTable(element.getSelectionModel().getSelectedItem());
-
-    }
-
-    public void btnPathUpAction(ActionEvent actionEvent) {
-        Path upperPath = Paths.get(pathField.getText()).getParent();
-        if (upperPath != null) {
-            updateFilesTable(upperPath);
-        }
-    }
-
-    public void surfDirectories(MouseEvent mouseEvent) {
-        Path path = Paths.get(pathField.getText()).resolve(filesTable.getSelectionModel().getSelectedItem().getFileName());
-        if ((mouseEvent.getClickCount() == 2) && (Files.isDirectory(path))) {
-            updateFilesTable(path);
-        }
-    }
-
-    public String getSelectedFileName(){
-        if (!filesTable.isFocused()){
-            return null;
-        } else {
-            return filesTable.getSelectionModel().getSelectedItem().getFileName();
-        }
-    }
-
-    public String getCurrentPath(){
-        return pathField.getText();
+        ComboBox<String> element = (ComboBox<String>) actionEvent.getSource();
+        updateFilesTable(Paths.get(element.getValue()));
     }
 }
-
